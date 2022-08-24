1b0cf54a25a91db1371aa1ab1c89516279edd59d
Jinping Wu <jinping.wu@verisilicon.com>
2022-08-24 15:28:18 +0800

    alius: add core register access function

--- src/target/alius/alius_aon_m33.c
+++ src/target/alius/alius_aon_m33.c
@@ -208,7 +208,7 @@ int aon_m33_read_memory_drw(uint32_t address, uint32_t *value) {
 	return retval;
 }
 
-int aon_m33_write_memory_dwr(uint32_t address, uint32_t value) {
+int aon_m33_write_memory_drw(uint32_t address, uint32_t value) {
 	int retval = ERROR_OK;
 
 	struct adiv5_ap *ap = dap_get_ap(global_dap, TOP_AON_APBAP);
@@ -236,10 +236,244 @@ int aon_m33_write_memory_dwr(uint32_t address, uint32_t value) {
 	return retval;
 }
 
-void alius_aon_m33_init_ap(struct adiv5_dap *dap) {
+int aon_m33_read_ahb_ap_register(uint32_t reg, uint32_t *value) {
+	int retval = ERROR_OK;
+
+	struct adiv5_ap *ap = dap_get_ap(global_dap, TOP_AON_APBAP);
+
+	//write aon tar. value is which ahb_ap reg we need to access
+	retval = dap_queue_ap_write(ap, AP_TAR, AON_M33_AHBAP + reg);
+	if (retval == ERROR_OK)
+		retval = dap_run(ap->dap);
+
+	//get the value
+	retval = dap_queue_ap_read(ap, AP_DRW, value);
+	if (retval == ERROR_OK)
+		retval = dap_run(ap->dap);
+
+	return retval;
+}
+
+int aon_m33_write_ahb_ap_register(uint32_t reg, uint32_t value) {
+	int retval = ERROR_OK;
+
+	struct adiv5_ap *ap = dap_get_ap(global_dap, TOP_AON_APBAP);
+
+	//write aon tar. value is which ahb_ap reg we need to access
+	retval = dap_queue_ap_write(ap, AP_TAR, AON_M33_AHBAP + reg);
+	if (retval == ERROR_OK)
+		retval = dap_run(ap->dap);
+
+	//write the value
+	retval = dap_queue_ap_write(ap, AP_DRW, value);
+	if (retval == ERROR_OK)
+		retval = dap_run(ap->dap);
+
+	return retval;
+}
+
+int aon_m33_enable_debug(void) {
+	uint32_t value, retval;
+
+	retval = aon_m33_read_memory_drw(DCB_DHCSR, &value);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+
+	value = DBGKEY | C_DEBUGEN;
+	retval = aon_m33_write_memory_drw(DCB_DHCSR, value);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+
+	/* clear ns bit for access secure region */
+	retval = aon_m33_read_ahb_ap_register(AP_CSW, &value);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+	value &= (~CSW_HNONSEC);
+	retval = aon_m33_write_ahb_ap_register(AP_CSW, value);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+
+	return ERROR_OK;
+}
+
+void alius_aon_m33_init(struct adiv5_dap *dap) {
+	uint32_t retval;
+
 	dap->ap[0].ap_num = TOP_BASE;
 	dap->ap[1].ap_num = AON_M33_AHBAP;
 	dap->adi_version = 6;
+	retval = aon_m33_enable_debug();
+	if(retval != ERROR_OK) {
+		LOG_OUTPUT("NOTICE: init fail\n");
+	}
+}
+
+int aon_m33_halt(void) {
+	uint32_t value, retval;
+
+	retval = aon_m33_read_memory_drw(DCB_DHCSR, &value);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+
+	value &= ~(0xFFFFul << 16);
+	value |= DBGKEY | C_HALT | C_DEBUGEN;
+	retval = aon_m33_write_memory_drw(DCB_DHCSR, value);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+	return ERROR_OK;
+}
+
+int aon_m33_unhalt(void) {
+	uint32_t value, retval;
+
+	retval = aon_m33_read_memory_drw(DCB_DHCSR, &value);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+
+	/* clear debug key and halt */
+	value &= ~((0xFFFFul << 16) | C_HALT);
+	/* add debug key and enable debug */
+	value |= DBGKEY | C_DEBUGEN;
+
+	retval = aon_m33_write_memory_drw(DCB_DHCSR, value);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+	return ERROR_OK;
+}
+
+int aon_m33_read_core_reg(uint32_t regsel, uint32_t* value) {
+	uint32_t tmp_value, retval;
+	int64_t time;
+
+	/* halt for enter debug state to read*/
+	retval = aon_m33_halt();
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+
+	/* setup select */
+	retval = aon_m33_write_memory_drw(DCB_DCRSR, regsel);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+
+	time = timeval_ms();
+	while(1) {
+
+		retval = aon_m33_read_memory_drw(DCB_DHCSR, &tmp_value);
+		if(retval != ERROR_OK) {
+			return retval;
+		}
+
+		if(tmp_value & S_REGRDY)//data ok
+			break;
+
+		if (timeval_ms() > time + DHCSR_S_REGRDY_TIMEOUT) {
+			LOG_OUTPUT("Timeout waiting for DCRDR transfer ready");
+			return ERROR_TIMEOUT_REACHED;
+		}
+	}
+
+	retval = aon_m33_read_memory_drw(DCB_DCRDR, value);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+
+	return ERROR_OK;
+}
+
+int aon_m33_write_core_reg(uint32_t regsel, uint32_t value) {
+	uint32_t tmp_value, retval;
+	int64_t time;
+
+	/* halt for enter debug state to read*/
+	retval = aon_m33_halt();
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+
+	/* write value to data register */
+	retval = aon_m33_write_memory_drw(DCB_DCRDR, value);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+
+	/* setup select */
+	regsel |= DCRSR_WNR;
+	retval = aon_m33_write_memory_drw(DCB_DCRSR, regsel);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+
+	/* wait for operate done */
+	time = timeval_ms();
+	while(1) {
+
+		retval = aon_m33_read_memory_drw(DCB_DHCSR, &tmp_value);
+		if(retval != ERROR_OK) {
+			return retval;
+		}
+
+		if(tmp_value & S_REGRDY)//data ok
+			break;
+
+		if (timeval_ms() > time + DHCSR_S_REGRDY_TIMEOUT) {
+			LOG_OUTPUT("Timeout waiting for DCRDR transfer ready");
+			return ERROR_TIMEOUT_REACHED;
+		}
+	}
+
+	return ERROR_OK;
+}
+
+int aon_m33_dump_regs(struct command_invocation *cmd) {
+	uint32_t tmp_value, retval;
+
+	/* halt for enter debug state to read*/
+	retval = aon_m33_halt();
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+
+	/* dump r0-r12 */
+	for(int i = 0; i < 13; i++) {
+		retval = aon_m33_read_core_reg(i, &tmp_value);
+		if(retval != ERROR_OK) {
+			return retval;
+		}
+		command_print(CMD, "r%d	: 0x%08x", i, tmp_value);
+	}
+
+	/* dump sp */
+	retval = aon_m33_read_core_reg(13, &tmp_value);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+	command_print(CMD, "sp	: 0x%08x", tmp_value);
+
+	/* dump lr */
+	retval = aon_m33_read_core_reg(14, &tmp_value);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+	command_print(CMD, "lr	: 0x%08x", tmp_value);
+
+	/* dump pc */
+	retval = aon_m33_read_core_reg(15, &tmp_value);
+	if(retval != ERROR_OK) {
+		return retval;
+	}
+	command_print(CMD, "pc	: 0x%08x", tmp_value);
+
+	return ERROR_OK;
 }
 
 COMMAND_HANDLER(handle_info_command)
@@ -258,7 +492,7 @@ COMMAND_HANDLER(handle_md_command)
 	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], address);
 
 	if(!ap_init) {
-		alius_aon_m33_init_ap(global_dap);
+		alius_aon_m33_init(global_dap);
 		ap_init = 1;
 	}
 
@@ -284,11 +518,11 @@ COMMAND_HANDLER(handle_mw_command)
 
 
 	if(!ap_init) {
-		alius_aon_m33_init_ap(global_dap);
+		alius_aon_m33_init(global_dap);
 		ap_init = 1;
 	}
 
-	retval = aon_m33_write_memory_dwr(address, value);
+	retval = aon_m33_write_memory_drw(address, value);
 	if(retval != ERROR_OK) {
 		command_print(CMD, "write 0x%08x fail", address);
 		return retval;
@@ -308,7 +542,7 @@ COMMAND_HANDLER(handle_md_dar_command)
 	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], address);
 
 	if(!ap_init) {
-		alius_aon_m33_init_ap(global_dap);
+		alius_aon_m33_init(global_dap);
 		ap_init = 1;
 	}
 
@@ -337,7 +571,7 @@ COMMAND_HANDLER(handle_mw_dar_command)
 	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], address);
 
 	if(!ap_init) {
-		alius_aon_m33_init_ap(global_dap);
+		alius_aon_m33_init(global_dap);
 		ap_init = 1;
 	}
 
@@ -364,7 +598,7 @@ COMMAND_HANDLER(handle_md_bd_command)
 	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], address);
 
 	if(!ap_init) {
-		alius_aon_m33_init_ap(global_dap);
+		alius_aon_m33_init(global_dap);
 		ap_init = 1;
 	}
 
@@ -393,7 +627,7 @@ COMMAND_HANDLER(handle_mw_bd_command)
 	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], address);
 
 	if(!ap_init) {
-		alius_aon_m33_init_ap(global_dap);
+		alius_aon_m33_init(global_dap);
 		ap_init = 1;
 	}
 
@@ -407,6 +641,235 @@ COMMAND_HANDLER(handle_mw_bd_command)
 	}
 
 	command_print(CMD, "done");
+	return ERROR_OK;
+}
+
+COMMAND_HANDLER(handle_dump_dcb_command)
+{
+	uint32_t value, retval;
+	if (CMD_ARGC != 0)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	if(!ap_init) {
+		alius_aon_m33_init(global_dap);
+		ap_init = 1;
+	}
+
+	/* DHCSR */
+	retval = aon_m33_read_memory_drw(DCB_DHCSR, &value);
+	if(retval != ERROR_OK) {
+		command_print(CMD, "read fail");
+		return retval;
+	}
+	command_print(CMD, "DCB_DHCSR: 0x%08x", value);
+
+	/* DCRSR */
+	retval = aon_m33_read_memory_drw(DCB_DCRSR, &value);
+	if(retval != ERROR_OK) {
+		command_print(CMD, "read fail");
+		return retval;
+	}
+	command_print(CMD, "DCB_DCRSR: 0x%08x", value);
+
+	/* DCRDR */
+	retval = aon_m33_read_memory_drw(DCB_DCRDR, &value);
+	if(retval != ERROR_OK) {
+		command_print(CMD, "read fail");
+		return retval;
+	}
+	command_print(CMD, "DCB_DCRDR: 0x%08x", value);
+
+	/* DCB_DEMCR */
+	retval = aon_m33_read_memory_drw(DCB_DEMCR, &value);
+	if(retval != ERROR_OK) {
+		command_print(CMD, "read fail");
+		return retval;
+	}
+	command_print(CMD, "DCB_DEMCR: 0x%08x", value);
+
+	/* DCB_DAUTHCTRL */
+	retval = aon_m33_read_memory_drw(DCB_DAUTHCTRL, &value);
+	if(retval != ERROR_OK) {
+		command_print(CMD, "read fail");
+		return retval;
+	}
+	command_print(CMD, "DCB_DAUTHCTRL: 0x%08x", value);
+
+	/* DCB_DSCSR */
+	retval = aon_m33_read_memory_drw(DCB_DSCSR, &value);
+	if(retval != ERROR_OK) {
+		command_print(CMD, "read fail");
+		return retval;
+	}
+	command_print(CMD, "DCB_DSCSR: 0x%08x", value);
+
+	return ERROR_OK;
+}
+
+COMMAND_HANDLER(handle_halt_command)
+{
+	uint32_t retval;
+	if (CMD_ARGC != 0)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	if(!ap_init) {
+		alius_aon_m33_init(global_dap);
+		ap_init = 1;
+	}
+
+	retval = aon_m33_halt();
+	if(retval != ERROR_OK) {
+		command_print(CMD, "halt core fail");
+		return retval;
+	}
+
+	command_print(CMD, "done");
+
+	return ERROR_OK;
+}
+
+COMMAND_HANDLER(handle_unhalt_command)
+{
+	uint32_t retval;
+	if (CMD_ARGC != 0)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	if(!ap_init) {
+		alius_aon_m33_init(global_dap);
+		ap_init = 1;
+	}
+
+	retval = aon_m33_unhalt();
+	if(retval != ERROR_OK) {
+		command_print(CMD, "unhalt core fail");
+		return retval;
+	}
+
+	command_print(CMD, "done");
+
+	return ERROR_OK;
+}
+
+COMMAND_HANDLER(handle_regr_command)
+{
+	uint32_t retval, regsel, value;
+
+	if (CMD_ARGC != 1)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], regsel);
+
+	if(!ap_init) {
+		alius_aon_m33_init(global_dap);
+		ap_init = 1;
+	}
+
+	retval = aon_m33_read_core_reg(regsel, &value);
+	if(retval != ERROR_OK) {
+		command_print(CMD, "read reg%d fail", regsel);
+		return retval;
+	}
+
+	command_print(CMD, "r%d : 0x%08x", regsel, value);
+
+	return ERROR_OK;
+}
+
+COMMAND_HANDLER(handle_regw_command)
+{
+	uint32_t retval, regsel, value;
+
+	if (CMD_ARGC != 2)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], regsel);
+	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[1], value);
+
+	if(!ap_init) {
+		alius_aon_m33_init(global_dap);
+		ap_init = 1;
+	}
+
+	retval = aon_m33_write_core_reg(regsel, value);
+	if(retval != ERROR_OK) {
+		command_print(CMD, "write reg%d fail", regsel);
+		return retval;
+	}
+
+	command_print(CMD, "write r%d : 0x%08x", regsel, value);
+
+	return ERROR_OK;
+}
+
+COMMAND_HANDLER(handle_regsdump_command)
+{
+	uint32_t retval;
+
+	if (CMD_ARGC != 0)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	if(!ap_init) {
+		alius_aon_m33_init(global_dap);
+		ap_init = 1;
+	}
+
+	retval = aon_m33_dump_regs(CMD);
+	if(retval != ERROR_OK) {
+		command_print(CMD, "dump core registers fail");
+		return retval;
+	}
+
+	return ERROR_OK;
+}
+
+COMMAND_HANDLER(handle_ap_reg_read_command)
+{
+	uint32_t retval, reg, value;
+
+	if (CMD_ARGC != 1)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], reg);
+
+	if(!ap_init) {
+		alius_aon_m33_init(global_dap);
+		ap_init = 1;
+	}
+
+	retval = aon_m33_read_ahb_ap_register(reg, &value);
+	if(retval != ERROR_OK) {
+		command_print(CMD, "apb ap read reg%d fail", reg);
+		return retval;
+	}
+
+	command_print(CMD, "0x%04x : 0x%08x", reg, value);
+
+	return ERROR_OK;
+}
+
+COMMAND_HANDLER(handle_ap_reg_write_command)
+{
+	uint32_t retval, reg, value;
+
+	if (CMD_ARGC != 2)
+		return ERROR_COMMAND_SYNTAX_ERROR;
+
+	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[0], reg);
+	COMMAND_PARSE_NUMBER(u32, CMD_ARGV[1], value);
+
+	if(!ap_init) {
+		alius_aon_m33_init(global_dap);
+		ap_init = 1;
+	}
+
+	retval = aon_m33_write_ahb_ap_register(reg, value);
+	if(retval != ERROR_OK) {
+		command_print(CMD, "apb ap write reg%d fail", reg);
+		return retval;
+	}
+
+	command_print(CMD, "0x%04x : 0x%08x", reg, value);
+
 	return ERROR_OK;
 }
 
@@ -459,6 +922,62 @@ const struct command_registration alius_aon_m33_command_handlers[] = {
 		.mode = COMMAND_ANY,
 		.usage = "address",
 		.help = "write 16 bytes memory, u32, use bd register, so make sure address bit[0:3] is 0",
+	},
+	{
+		.name = "dcb",
+		.handler = &handle_dump_dcb_command,
+		.mode = COMMAND_ANY,
+		.usage = "",
+		.help = "dump debug control block registers",
+	},
+	{
+		.name = "halt",
+		.handler = &handle_halt_command,
+		.mode = COMMAND_ANY,
+		.usage = "",
+		.help = "halt core, enter debug state",
+	},
+	{
+		.name = "unhalt",
+		.handler = &handle_unhalt_command,
+		.mode = COMMAND_ANY,
+		.usage = "",
+		.help = "unhalt core, let core run",
+	},
+	{
+		.name = "regr",
+		.handler = &handle_regr_command,
+		.mode = COMMAND_ANY,
+		.usage = "reg_number(0 for r0)",
+		.help = "read core register",
+	},
+	{
+		.name = "regw",
+		.handler = &handle_regw_command,
+		.mode = COMMAND_ANY,
+		.usage = "reg_number(0 for r0)",
+		.help = "write core register",
+	},
+	{
+		.name = "regsdump",
+		.handler = &handle_regsdump_command,
+		.mode = COMMAND_ANY,
+		.usage = "",
+		.help = "dump core registers",
+	},
+	{
+		.name = "ap_reg_read",
+		.handler = &handle_ap_reg_read_command,
+		.mode = COMMAND_ANY,
+		.usage = "address",
+		.help = "read ahb ap register, address is ahb ap reg offset",
+	},
+	{
+		.name = "ap_reg_write",
+		.handler = &handle_ap_reg_write_command,
+		.mode = COMMAND_ANY,
+		.usage = "address value",
+		.help = "write ahb ap register, address is ahb ap reg offset",
 	},
 	COMMAND_REGISTRATION_DONE
 };
--- src/target/alius/alius_aon_m33.h
+++ src/target/alius/alius_aon_m33.h
@@ -13,5 +13,35 @@
 #define AP_DRW 0x0D0C
 #define AP_BD0 0x0D10
 
+/* ns bit */
+#define CSW_HNONSEC BIT(30)
+
+/* DCB_DHCSR bit and field definitions */
+#define DBGKEY		(0xA05Ful << 16)
+#define C_DEBUGEN	BIT(0)
+#define C_HALT		BIT(1)
+#define C_STEP		BIT(2)
+#define C_MASKINTS	BIT(3)
+#define S_REGRDY	BIT(16)
+#define S_HALT		BIT(17)
+#define S_SLEEP		BIT(18)
+#define S_LOCKUP	BIT(19)
+#define S_RETIRE_ST	BIT(24)
+#define S_RESET_ST	BIT(25)
+
+//Debug Control Block
+#define DCB_DHCSR 0xE000EDF0  /*Debug Halting Control and Status Register*/
+#define DCB_DCRSR 0xE000EDF4  /*Debug Core Register Select Register*/
+#define DCB_DCRDR 0xE000EDF8  /*Debug Core Register Data Register*/
+#define DCB_DEMCR 0xE000EDFC  /*Debug Exception and Monitor Control Register*/
+#define DCB_DAUTHCTRL 0xE000EE04  /*Debug Authentication Control Register*/
+#define DCB_DSCSR 0xE000EE08  /*Debug Security Control and Status Register*/
+
+#define DAUTHSTATUS 0xE000EFB8  /*Debug Authentication Status Register*/
+
+#define DCRSR_WNR	BIT(16)
+
+#define DHCSR_S_REGRDY_TIMEOUT (500)
+
 extern const struct command_registration alius_aon_m33_command_handlers[];
 #endif /* OPENOCD_ALIUS_AON_M33_H */
